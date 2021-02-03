5b393fdc763538572619d0a948ad6d03bd84dab5
hotire <gngh0101@gmail.com>
2021-02-04 00:08:49 +0900

    Add V1Employee

---
+++ spring-rest-react-basic/src/main/java/com/github/hotire/spring/rest/react/employee/V1Employee.java
@@ -0,0 +1,11 @@
+package com.github.hotire.spring.rest.react.employee;
+
+import lombok.Data;
+
+@Data
+public class V1Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String description;
+}
